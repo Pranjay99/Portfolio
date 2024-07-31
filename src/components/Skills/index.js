1d02f977cb4a2834bbce486a1b8a2ce6be8ce2f8
Pranjay99 <93260737+Pranjay99@users.noreply.github.com>
2024-07-31 14:29:06 +0530

    barbel changed

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -152,7 +152,7 @@ const Skills = () => {
       <Wrapper>
         <Title>Skills</Title>
         <Desc>
-          Here are some of my skills on which I have been working on for the past 2 years.
+          Here are some of my skills on which I have been working on for the past 3 years.
         </Desc>
         <SkillsContainer>
           {skills.map((skill, index) => (
